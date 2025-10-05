ceabf1a977b32a65be9b39dd9e690c837aa77387
med1wsg <omersamigul@gmail.com>
2025-10-05 19:38:00 +0300

    ticket kanalının id si eklendi

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ client.on('interactionCreate', async interaction => {
         const channel = await interaction.guild.channels.create({
             name: ticketChannelName,
             type: ChannelType.GuildText,
-            parent: null, // İsteğe bağlı: Ticket kategorisi ID'sini buraya yazabilirsin. Şimdilik kategorisiz kalsın.
+            parent: 1420481602387054693, // Ticket kategorisinin ID'sini buraya girin
             permissionOverwrites: [
                 {
                     // Herkesin izinlerini ayarla (kanalı görmesinler)
