6ca12b380df40a14a3bb999ecd03e77a36398648
med1wsg <omersamigul@gmail.com>
2025-10-09 20:06:47 +0300

    kaybetme bunu

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ keepAlive();
 // LOG KANALININ ID'sini buraya GİRİN!
 // Bu ID'yi kendi log kanalınızın ID'siyle değiştirin.
 const LOG_CHANNEL_ID = "1376137436391804938"; 
+const LOG_CHANNEL_ID2 = "1274334627686781020"; // İkinci log kanalı ID'si
 
 
 // Tokeni .env dosyasından güvenli bir şekilde çeker
